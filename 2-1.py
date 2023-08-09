6686552ee2a91c9e2d8187de85d1a07ba27008a6
yhp2205 <yhp2205@gmail.com>
2023-08-09 15:11:43 +0900

    0809 al

--- 2-1.py
+++ 2-1.py
@@ -12,3 +12,17 @@ def find_max(a):
 
 v = [17, 92, 18, 33, 58, 7, 33, 42]
 print(find_max(v))
+
+#최댓값 위치 구하기
+
+def find_max_idx(a):
+    n = len(a)
+    max_idx = 0
+    for i in range(1,n):
+        if a[i] > a[max_idx]:
+            max_idx = i
+    return max_idx
+
+
+v = [17, 92, 18, 33, 58, 7, 33, 42]
+print(find_max_idx(v))
