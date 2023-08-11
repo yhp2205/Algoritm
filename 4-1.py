a9a3034f7c030c15f00cf4ccb58a973dabf833e1
yhp2205 <yhp2205@gmail.com>
2023-08-11 13:06:40 +0900

    0811

--- 4-1.py
+++ 4-1.py
@@ -10,3 +10,14 @@ def fact(n):
 print(fact(1))
 print(fact(5))
 print(fact(10))
+
+#연속한 숫자의 곱을 구하는 알고리즘 2
+
+def fact(n):
+    if n <=1:
+        return 1
+    return n*fact(n-1)
+
+print(fact(1))
+print(fact(5))
+print(fact(10))
